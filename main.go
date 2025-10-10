daf8a2cb09d9e71f338fd0b3b5aea769f0de8554
raihanuldev <rihanthedev@gmail.com>
2025-10-10 09:49:11 +0600

    response Header Modifed

--- main.go
+++ main.go
@@ -28,6 +28,9 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 
 // Api for Get all Products
 func getProducts(w http.ResponseWriter, r *http.Request) {
+	// response config
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	if r.Method != "GET" {
 		http.Error(w, "Please Sent GET Request", 400)
 		return
