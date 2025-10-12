8549df82653796aa50c36843670c707680e79cf7
raihanuldev <rihanthedev@gmail.com>
2025-10-12 21:09:12 +0600

    PreFlightError: Fixed Cross Origin Issues with an OPTIONS Method

--- main.go
+++ main.go
@@ -6,23 +6,25 @@ import (
 	"net/http"
 )
 
-// GLobal Varibale
+// GLobal Varibal
 var productList []Product
 
 // Struct
 //
 //	Product Struct
 type Product struct {
-	ID          int     `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	ImgUrl      string  `json:"imageUrl"`
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Price       string `json:"price"`
+	ImgUrl      string `json:"imageUrl"`
 }
 
 // Resubale Function
 func handleCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST,OPTIONS,PATCH,PUT,DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
 }
 func sendData(w http.ResponseWriter, data interface{}, statusCode int) {
@@ -40,6 +42,11 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 // Api for Get all Products
 func getProducts(w http.ResponseWriter, r *http.Request) {
 	handleCors(w)
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(200)
+		return
+	}
 	if r.Method != "GET" {
 		http.Error(w, "Please Sent GET Request", 400)
 		return
@@ -51,6 +58,10 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 // Create Product
 func createProduct(w http.ResponseWriter, r *http.Request) {
 	handleCors(w)
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	//cheking if request are vaild or invaild
 	if r.Method != "POST" {
 		http.Error(w, "Please Sent POST Request", 400)
@@ -62,7 +73,7 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&newProduct)
 
 	if err != nil {
-		fmt.Println("Error")
+		fmt.Println("Error", err)
 		http.Error(w, "Please give  me a Vaild json", 400)
 		return
 	}
@@ -76,14 +87,11 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-
-	// REST APIS
-	// mux.HandleFunc("/hello", handleHello)
 	mux.Handle("GET /hello", http.HandlerFunc(handleHello))
-	// mux.HandleFunc("/products", getProducts)
+	mux.Handle("OPTIONS /products", http.HandlerFunc(getProducts))
 	mux.Handle("GET /products", http.HandlerFunc(getProducts))
-	// mux.HandleFunc("/create-product", createProduct)
 	mux.Handle("POST /create-product", http.HandlerFunc(createProduct))
+	mux.Handle("OPTIONS /create-product", http.HandlerFunc(createProduct))
 
 	//Server Config
 	fmt.Println("Server is running on port, 3000")
@@ -100,42 +108,42 @@ func init() {
 		ID:          1,
 		Title:       "headphone",
 		Description: "This is Best Quality Headphone i have ever seen",
-		Price:       199.21,
+		Price:       "199.21",
 		ImgUrl:      "https/////image.url",
 	}
 	pr2 := Product{
 		ID:          2,
 		Title:       "VIVO",
 		Description: "This is Best Quality VIVO Phone i have ever seen",
-		Price:       199.21,
+		Price:       "199.21",
 		ImgUrl:      "https/////image.url",
 	}
 	pr3 := Product{
 		ID:          3,
 		Title:       "APPLE",
 		Description: "This is Best Quality Headphone i have ever seen",
-		Price:       199.21,
+		Price:       "199.21",
 		ImgUrl:      "https/////image.url",
 	}
 	pr4 := Product{
 		ID:          4,
 		Title:       "SoundBox",
 		Description: "This is Best Quality SoundBox i have ever seen",
-		Price:       199.21,
+		Price:       "199.21",
 		ImgUrl:      "https/////image.url",
 	}
 	pr5 := Product{
 		ID:          5,
 		Title:       "Laptop",
 		Description: "This is Best Quality Laptop i have ever seen",
-		Price:       199.21,
+		Price:       "199.21",
 		ImgUrl:      "https/////image.url",
 	}
 	pr6 := Product{
 		ID:          6,
 		Title:       "Monitor",
 		Description: "This is Best Quality Monitor i have ever seen",
-		Price:       199.21,
+		Price:       "199.21",
 		ImgUrl:      "https/////image.url",
 	}
 	// for Static i will apend in slice
